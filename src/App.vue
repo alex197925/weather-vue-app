f66cd50a07edd1b414e500442e298e070e0c88e4
Tolok Oleksandr <tolokoleksandr@Toloks-MacBook-Pro.local>
2023-04-28 13:08:31 +0200

    Added input,button: style

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
   <div class="wrapper">
     <h1>Weather App</h1>
     <p>Weather in your City</p>
+    <input type="text" placeholder="City..." />
+    <button>Get Weather</button>
   </div>
 </template>
 
@@ -21,5 +23,42 @@
 
 .wrapper h1 {
   margin-top: 50px;
+  letter-spacing: 1px;
+}
+
+.wrapper p {
+  padding-top: 20px;
+  letter-spacing: 1px;
+}
+
+.wrapper input {
+  margin-top: 30px;
+  padding: 5px 8px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #370202;
+  color: #fcfcfc;
+  font-size: 14px;
+  outline: none;
+}
+
+.wrapper input:focus {
+  border-bottom-color: #e3bc4d;
+}
+
+.wrapper button {
+  background: #e3bc4d;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  border: none;
+  transition: transform 500ms ease;
+}
+
+.wrapper button:hover {
+  transform: scale(1.1) translateY(-5px);
 }
 </style>
