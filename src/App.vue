e542daa7e3842b9d16f0059b9c473d18524d6ed4
Tolok Oleksandr <tolokoleksandr@Toloks-MacBook-Pro.local>
2023-04-28 12:49:35 +0200

    Add content to the App.vue: title, style

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
+<!-- @format -->
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="wrapper">
+    <h1>Weather App</h1>
+    <p>Weather in your City</p>
+  </div>
 </template>
 
+<script></script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.wrapper {
+  width: 900px;
+  height: 500px;
+  padding: 20px;
+  text-align: center;
+  color: #fff;
+  border-radius: 50px;
+  background-color: #621022;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.wrapper h1 {
+  margin-top: 50px;
 }
 </style>
